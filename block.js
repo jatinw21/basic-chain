ca59f309ede2bf885cc658ce82d5f85502d55813
Jatin Wadhwa <jatinw21@gmail.com>
2018-07-21 00:52:37 +1000

    Block class initial version

--- block.js
+++ block.js
@@ -1,8 +1,16 @@
-class Block {
+export default class Block {
     constructor(timestamp, lastHash, hash, data) {
         this.timestamp = timestamp;
         this.lastHash = lastHash;
         this.hash = hash;
         this.data = data;
+    }
+
+    toString() {
+        return `Block Info - 
+            Timestamp : ${this.timestamp}
+            Last Hash : ${this.lastHash.substring(0,10)}
+            Hash      : ${this.hash.substring(0,10)}
+            Data      : ${this.data}`;
     }
 }
