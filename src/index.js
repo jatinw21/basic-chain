ef53fca726ca5d24faaf6ea54e668c2af2513fb7
Jatin Wadhwa <jatinw21@gmail.com>
2018-07-21 12:07:52 +1000

    small cleanup

--- src/index.js
+++ src/index.js
@@ -11,23 +11,14 @@ const block = new Block('test1', 'test2', 'test3', 'test4')
 // console.log(Block.genesis().toString())
 // console.log("=====")
 
-const fooBlock = Block.mineBlock(Block.genesis(), "foo");
-console.log("\n=====")
-console.log(fooBlock.toString())
-console.log("=====")
+// const fooBlock = Block.mineBlock(Block.genesis(), "foo");
+// console.log("\n=====")
+// console.log(fooBlock.toString())
+// console.log("=====")
 
 const server = http.createServer(app)
 let currentApp = app
 server.listen(3000)
-
-
-
-
-
-
-
-
-
 
 
 if (module.hot) {
--- src/server.js
+++ src/server.js
@@ -1,8 +1,8 @@
 import express from 'express'
 const app = express()
-app.get('/api', (req, res) => {
+app.get('/', (req, res) => {
     res.send({
-        message: 'I am a server route and can also be hot reloaded!'
+        message: 'Heelo, World. Hot reloadable!'
     })
 })
 export default app
