559a6e7bc241abdee80331c1f6b58acad4f67044
Jatin Wadhwa <jatinw21@gmail.com>
2018-07-22 22:25:38 +1000

    Mining post request endpoint to add new blocks

--- src/app/app.js
+++ src/app/app.js
@@ -1,12 +1,21 @@
 import express from 'express'
+import bodyParser from 'body-parser'
 import Blockchain from '../blockchain'
 
 const app = express()
 
 const bc = new Blockchain()
 
+app.use(bodyParser.json());
+
 app.get('/blocks', (req, res) => {
     res.json(bc.chain)    
+})
+
+app.post('/mine', (req, res) => {
+    const block = bc.addBlock(req.body.data)
+    console.log(`New block added: ${block.toString()}`)
+    res.redirect('/blocks')  
 })
 
 export default app
