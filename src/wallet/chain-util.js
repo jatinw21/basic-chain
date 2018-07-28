8239d760d1618531198bb4b1f8a440d4ee25016d
Jatin Wadhwa <jatinw21@gmail.com>
2018-07-28 12:49:41 +1000

    method using uuid to generate unique timestamp based ids

--- src/wallet/chain-util.js
+++ src/wallet/chain-util.js
@@ -1,9 +1,14 @@
 import { ec as EC } from 'elliptic'
+import uuidV1 from 'uuid/v1'
 
 const ec = new EC('secp256k1')
 
 export default class ChainUtil {
     static genKeyPair() {
         return ec.genKeyPair();
+    }
+
+    static id() {
+        return uuidV1();
     }
 }
